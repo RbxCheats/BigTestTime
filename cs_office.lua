536332fb5fbac1b880bed201e08926ffdb794d8c
Jmuse <60051610+RbxCheats@users.noreply.github.com>
2020-04-25 19:23:36 -0500

    Add files via upload

---
+++ cs_office.lua
@@ -0,0 +1,8 @@
+skych = game:service("Lighting").Sky
+
+skych.SkyboxBk = "rbxassetid://658623433"
+skych.SkyboxDn = "rbxassetid://316342560"
+skych.SkyboxFt = "rbxassetid://658625205"
+skych.SkyboxLf = "rbxassetid://658627155"
+skych.SkyboxRt = "rbxassetid://658628504"
+skych.SkyboxUp = "rbxassetid://658632701"
